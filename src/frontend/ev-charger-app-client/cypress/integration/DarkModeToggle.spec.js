e6f9545c61041ea54a0b09c61621225b45a0f4de
Talpos Norbert <norberttalpos@gmail.com>
2022-05-05 08:40:55 +0200

    added cypress to vue

---
+++ src/frontend/ev-charger-app-client/cypress/integration/DarkModeToggle.spec.js
@@ -0,0 +1,15 @@
+import { mount } from '@cypress/vue'
+import DarkModeToggle from './DarkModeToggle.vue'
+
+describe('HelloWorld', () => {
+  it('renders a message', () => {
+    const msg = 'Hello Cypress Component Testing!'
+    mount(DarkModeToggle, {
+      propsData: {
+        msg
+      }
+    })
+
+    cy.get('h1').should('have.text', msg)
+  })
+})
---
+++ src/frontend/ev-charger-app-client/cypress/plugins/index.js
@@ -0,0 +1,11 @@
+const { startDevServer } = require('@cypress/webpack-dev-server')
+
+module.exports = (on, config) => {
+  on('dev-server:start', options =>
+    startDevServer({
+      options,
+    })
+  )
+
+  return config
+}
